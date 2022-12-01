9743f532d73c0d05f71c27282206a2f283ff4fe2
Danny Echeverri <decheverri123@gmail.com>
2022-12-01 17:14:04 -0500

    tab stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { MoviesComponent } from './pages/movies/movies.component';
-import { HttpClientModule } from '@angular/common/http';
-import { SliderComponent } from './components/slider/slider.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
 import { ItemComponent } from './components/item/item.component';
+import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { HomeComponent } from './pages/home/home.component';
 import { MovieComponent } from './pages/movie/movie.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { MovieComponent } from './pages/movie/movie.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
