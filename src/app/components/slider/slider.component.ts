30bc3bf6127e4ba570f976a4c57ee8b1b20979a4
Danny Echeverri <decheverri123@gmail.com>
2022-12-01 14:25:26 -0500

    fade animation

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
 
@@ -5,6 +12,13 @@ import { Movie } from '../../models/movie';
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void => *', [animate('1s')]),
+      transition('* => void', [animate('500ms')]),
+    ]),
+  ],
 })
 export class SliderComponent implements OnInit {
   @Input() items!: Movie[];
